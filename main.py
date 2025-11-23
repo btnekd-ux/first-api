efa81f71d8ce3a9c3828cdd304405b285ca55a00
btnekd-ux <btnekd@gmail.com>
2025-11-23 23:18:56 +0600

    post and get function

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from pydantic import BaseModel, EmailStr
 import uvicorn # type: ignore
 
 app = FastAPI()
+
+class createUser(BaseModel):
+    emeil: EmailStr
+    
+    
 
 @app.get("/")
 def hello_index():
     return {
         "message": "Hello, index!"
     }
+    
+    
+@app.get("/hello/")
+def hello(name: str):
+    name = name.strip.title()
+    return {"message": f"Hello, {name}!"}
+
+
+@app.get("/users/")
+def create_user(user: createUser):
+    return{ 
+        "message": "success",
+        "email": user.email,
+    }
+    
+    
+    
+@app.post("/calc/add/")
+def add(a: int, b: int):
+    return {
+        a: a,
+        b: b,
+        "result": a + b,
+    }   
+    
+
     
 @app.get("/item/")
 def list_items():
